8c264372d31b8ece8a8632e25111f0ed3657946b
Gaurav Agarwal <algogrit@gmail.com>
2019-02-25 18:51:17 +0530

    Updating the concurrency example.

--- examples/concurrency.go
+++ examples/concurrency.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"io/ioutil"
+	"strconv"
 )
 
-func say(s string, wg *sync.WaitGroup) {
-	for i := 0; i < 500; i++ {
-		for j := 0; j < 500; j++ {
-			mul := i * j
-			fmt.Println(mul)
+// func say(s string, index int, wg *sync.WaitGroup) {
+func say(s string, index int) {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			mul := []byte(strconv.Itoa(i * j))
+			ioutil.WriteFile("/dev/null", mul, 0644)
 		}
 	}
 
-	wg.Done()
+	fmt.Printf("Hello, %s! from %d goroutine\n", s, index+1)
+
+	// wg.Done()
 }
 
 func main() {
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
 	for i := 0; i < 4; i++ {
-		go say("world", &wg)
-		wg.Add(1)
+		say("world", i)
+		// go say("world", i, &wg)
+		// wg.Add(1)
 	}
 
-	wg.Wait()
+	// wg.Wait()
 }
