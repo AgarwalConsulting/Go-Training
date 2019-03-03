550892ef60303935bfc3e8b09ac387331c05d48f
Gaurav Agarwal <algogrit@gmail.com>
2019-03-03 10:27:38 +0530

    Adding generate example.

---
+++ examples/generate/README.md
@@ -0,0 +1,8 @@
+Generate Example
+================
+
+To run:
+
+	$ go generate
+	$ go build
+	$ ./generate
---
+++ examples/generate/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+//go:generate go run gen.go
+
+func main() {
+	fmt.Println(strFunc())
+}
