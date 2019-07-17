49040b423a02a979413c793f10f3f4adbe3db49a
Gaurav Agarwal <algogrit@gmail.com>
2019-07-17 14:31:22 +0530

    Formatting.

--- patterns/concurrency/06-pub-sub.go
+++ patterns/concurrency/06-pub-sub.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"sync"
 	"time"
 )
@@ -9,48 +9,48 @@ import (
 type Subscriber func(int)
 
 type PubSub struct {
-    c chan int
-    Subscribers []Subscriber
-    lock sync.RWMutex
+	c           chan int
+	Subscribers []Subscriber
+	lock        sync.RWMutex
 }
 
 func (pubSub *PubSub) Subscribe(subscriber Subscriber) {
-    pubSub.lock.Lock()
-    pubSub.Subscribers = append(pubSub.Subscribers, subscriber)
-    pubSub.lock.Unlock()
+	pubSub.lock.Lock()
+	pubSub.Subscribers = append(pubSub.Subscribers, subscriber)
+	pubSub.lock.Unlock()
 }
 
 func (pubSub *PubSub) Send(data int) {
-    pubSub.c <- data
+	pubSub.c <- data
 }
 
 func CreateNewPubSub() *PubSub {
-    var pubSub PubSub
-    pubSub.c = make(chan int)
+	var pubSub PubSub
+	pubSub.c = make(chan int)
 
-    go func(pubSub *PubSub) {
-        for {
-            data := <- pubSub.c
+	go func(pubSub *PubSub) {
+		for {
+			data := <-pubSub.c
 
-            pubSub.lock.RLock()
-            for _, subscriber := range pubSub.Subscribers {
-                subscriber(data)
-            }
-            pubSub.lock.RUnlock()
-        }
-    }(&pubSub)
+			pubSub.lock.RLock()
+			for _, subscriber := range pubSub.Subscribers {
+				subscriber(data)
+			}
+			pubSub.lock.RUnlock()
+		}
+	}(&pubSub)
 
-    return &pubSub
+	return &pubSub
 }
 
 func main() {
-    pubSub := CreateNewPubSub()
+	pubSub := CreateNewPubSub()
 
-    pubSub.Subscribe(func(data int) {
-        fmt.Println("Received new data: ", data)
-    })
+	pubSub.Subscribe(func(data int) {
+		fmt.Println("Received new data: ", data)
+	})
 
-    pubSub.Send(42)
+	pubSub.Send(42)
 
-    time.Sleep(1 * time.Millisecond)
+	time.Sleep(1 * time.Millisecond)
 }
