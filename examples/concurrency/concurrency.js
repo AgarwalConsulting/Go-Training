f707cc89a4577fe5c1b548d7ac5ae8bf705cb2ab
Gaurav Agarwal <algogrit@gmail.com>
2019-09-12 17:08:33 +0530

    Updating JS code.

--- examples/concurrency/concurrency.js
+++ examples/concurrency/concurrency.js
@@ -1,11 +1,28 @@
-var promise =  new Promise(function(resolve, reject) {
-  console.log("Hello from promise!");
+function busyWork(id) {
+  var iterCount = 30000;
+  for(var i = 0; i < iterCount; i++) {
+    for(var j = 0; j < iterCount; j++) {
+      mul = i * j;
+    }
+  }
+  console.log("Completed work: ", id);
+}
+
+// busyWork(1);
+// busyWork(2);
+// busyWork(3);
+
+var promise = new Promise(function(resolve, reject) {
+  busyWork(1);
+  console.log("1. Hello from promise!");
 
   resolve(42);
 });
 
 promise.then((val) => {
-  console.log("Processed: ", val);
+  busyWork(3);
+  console.log("2. Processed: ", val);
 });
 
-console.log("Hello, world!");
+busyWork(2);
+console.log("3. Hello, world!");
