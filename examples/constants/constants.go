5a42eb9d2cc6d4c2f2139866003f3fe913f40c0a
Gaurav Agarwal <algogrit@gmail.com>
2020-01-27 14:18:19 +0530

    Expanding the constants example.

--- examples/constants/constants.go
+++ examples/constants/constants.go
@@ -5,14 +5,22 @@ import (
 	"time"
 )
 
+type status int
+
+const (
+	single status = iota
+	engaged
+	married
+)
+
 func main() {
-	fmt.Println(time.Duration(6000))
-	// overflow := time.Saturday + 1
+	// s := status(1)
+	// day := time.Weekday(7) % 7
 
-	// fmt.Printf("%v, %#v\n", overflow, overflow)
+	// fmt.Println("The status is: ", s)
+	// fmt.Printf("%v, %T\n", s, s)
+	// fmt.Println("The day is: ", day)
+	// fmt.Printf("%v, %T\n", day, day)
 
-	// fmt.Println(time.Month(5))
-
-	// fmt.Println(time.Duration(6000000))
-	// fmt.Println(24 * time.Hour)
+	fmt.Println(time.Duration(1000000))
 }
