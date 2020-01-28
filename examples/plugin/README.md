c020d90675689d29011f6ead810eaa613e3a1fdb
Gaurav Agarwal <algogrit@gmail.com>
2020-01-28 09:54:37 +0530

    Adding example for plugin.

---
+++ examples/plugin/README.md
@@ -0,0 +1,19 @@
+# Plugin
+
+## Compile plugin - generate `.so` file
+
+```
+go build -buildmode=plugin plugin.go
+```
+
+## Run the example
+
+```bash
+go run hello.go
+```
+
+---
+
+## References
+
+1. [package plugin](https://golang.org/pkg/plugin/)
---
+++ examples/plugin/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "plugin"
+
+func main() {
+	p, err := plugin.Open("plugin.so")
+	if err != nil {
+		panic(err)
+	}
+	v, err := p.Lookup("V")
+	if err != nil {
+		panic(err)
+	}
+	f, err := p.Lookup("F")
+	if err != nil {
+		panic(err)
+	}
+	*v.(*int) = 7
+	f.(func())() // prints "Hello, number 7"
+	type Symbol interface{}
+}
