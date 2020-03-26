4888dc7bf6aefd05423fd82abf4e881d04c7915e
Gaurav Agarwal <algogrit@gmail.com>
2020-03-26 15:12:47 +0530

    Adding constructor definitions.

--- exercism/paasio/paasio.go
+++ exercism/paasio/paasio.go
@@ -1,1 +1,22 @@
 package paasio
+
+import (
+	"io"
+)
+
+// Define a type which implements ReadCounter, WriteCounter and ReadWriteCounter
+
+// NewReadWriteCounter returns a type which implements ReadWriteCounter
+func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
+	return nil
+}
+
+// NewReadCounter returns a type which implements ReadCounter
+func NewReadCounter(r io.Reader) ReadCounter {
+	return nil
+}
+
+// NewWriteCounter returns a type which implements WriteCounter
+func NewWriteCounter(r io.Writer) WriteCounter {
+	return nil
+}
