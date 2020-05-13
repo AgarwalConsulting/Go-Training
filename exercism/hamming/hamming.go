d90ca6ba073c4ebf052de38553561a92d15092cd
Gaurav Agarwal <algogrit@gmail.com>
2020-05-13 13:16:28 +0530

    Fixing typo.

--- exercism/hamming/hamming.go
+++ exercism/hamming/hamming.go
@@ -3,5 +3,5 @@ package hamming
 import "errors"
 
 func Distance(a, b string) (int, error) {
-	return 0, errors.New("If no error, replace error.New() with nil!")
+	return 0, errors.New("If no error, replace errors.New() with nil!")
 }
