e431e7e6b509383985255630c06d54415115bd14
Gaurav Agarwal <algogrit@gmail.com>
2020-06-21 17:53:55 +0530

    Grpc logging.

--- examples/net/grpc/fib/cmd/server/main.go
+++ examples/net/grpc/fib/cmd/server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"net"
+	"time"
 
 	pb "algogrit.com/fib-grpc/fibonacci"
 	log "github.com/sirupsen/logrus"
@@ -19,11 +21,49 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	// s := grpc.NewServer()
+	s := grpc.NewServer(withServerUnaryInterceptor(), grpc.StreamInterceptor(streamInterceptor))
 	pb.RegisterFibonacciServer(s, NewFibonacciServer())
 
 	log.Info("Starting fib server on port ", port, "...")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+// type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *UnaryServerInfo, handler UnaryHandler) (resp interface{}, err error)
+
+func serverInterceptor(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	start := time.Now()
+
+	// Calls the handler
+	h, err := handler(ctx, req)
+
+	log.Infof("Request - Method:%s\tDuration:%s\tError:%v\n",
+		info.FullMethod,
+		time.Since(start),
+		err)
+
+	return h, err
+}
+
+func withServerUnaryInterceptor() grpc.ServerOption {
+	return grpc.UnaryInterceptor(serverInterceptor)
+}
+
+// type StreamServerInterceptor func(srv interface{}, ss ServerStream, info *StreamServerInfo, handler StreamHandler) error
+
+func streamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	start := time.Now()
+
+	log.Info("Streaming: ", info.FullMethod)
+
+	err := handler(srv, ss)
+
+	log.Infof("Request - Method: %s\tDuration:%s\tError:%v\n", info.FullMethod, time.Since(start), err)
+
+	return err
 }
