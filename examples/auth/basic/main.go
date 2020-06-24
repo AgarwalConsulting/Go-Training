bd2d2f5be275c3cec8d763538823ce63a85d2036
Gaurav Agarwal <algogrit@gmail.com>
2020-06-25 01:07:05 +0530

    Fixing basic auth.

--- examples/auth/basic/main.go
+++ examples/auth/basic/main.go
@@ -1,45 +1,35 @@
 package main
 
 import (
-	"crypto/subtle"
 	"net/http"
 )
-
-// BasicAuth wraps a handler requiring HTTP basic auth for it using the given
-// username and password and the specified realm, which shouldn't contain quotes.
-//
-// Most web browser display a dialog with something like:
-//
-//    The website says: "<realm>"
-//
-// Which is really stupid so you may want to set the realm to a message rather than
-// an actual realm.
-func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		user, pass, ok := r.BasicAuth()
-
-		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
-			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
-			w.WriteHeader(401)
-			w.Write([]byte("Unauthorised.\n"))
-			return
-		}
-
-		handler(w, r)
-	}
-}
 
 const (
 	username = "admin"
 	password = "123456"
 )
 
+func BasicAuth(username, password, realm string, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+		iusername, ipassword, _ := req.BasicAuth()
+
+		if username != iusername || password != ipassword {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+
+		next(w, req)
+	}
+}
+
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world!"))
 }
 
 func main() {
-	http.HandleFunc("/", BasicAuth(handleIndex, username, password, "Please enter your username and password for this site"))
+	http.HandleFunc("/", BasicAuth(username, password, "Please enter your username and password for this site", handleIndex))
 
 	http.ListenAndServe(":9000", nil)
 }
