f55be84e4abfa25bbbd8f6050985d38272a539b8
Gaurav Agarwal <algogrit@gmail.com>
2020-06-28 21:00:01 +0530

    Adding VSCode installation script.

---
+++ setup/vscode-debian.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo snap install --classic code
+
+## (or)
+
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+# sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+# sudo apt-get install apt-transport-https
+# sudo apt-get update
+# sudo apt-get install code
