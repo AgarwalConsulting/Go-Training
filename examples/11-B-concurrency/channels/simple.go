52ed3e71adc8f0b67ec11e33da38ce4e9f8dc4b6
Gaurav Agarwal <algogrit@gmail.com>
2020-07-14 10:51:58 +0530

    Updating simple channels example.

--- examples/11-B-concurrency/channels/simple.go
+++ examples/11-B-concurrency/channels/simple.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
 	var ch chan int
 	ch = make(chan int) // Unbuffered channel
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
+		defer wg.Done()
 		fmt.Println("Sending...")
-		ch <- 42 // Sending, is blocked until there is a receive
+		ch <- 42 // Sending to a channel is going to block until there is a receive
 		fmt.Println("Sent!")
 	}()
 
-	fmt.Println("Receiving...")
-	val := <-ch // Receiving, is blocked until there is a send
-	fmt.Println("Received: ", val)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Receiving...")
+		time.Sleep(time.Second * 3)
+		val := <-ch // Receiving from a channel is going to block until there is a send
+		fmt.Println("Recieved: ", val)
+	}()
+
+	wg.Wait()
+
+	// fmt.Println("Next: ", <-ch)
 }
