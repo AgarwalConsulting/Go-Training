fab559313016b72fa482a18950cc76a818b94e86
Gaurav Agarwal <algogrit@gmail.com>
2020-07-16 10:57:33 +0530

    Cleaning up main.

--- examples/third-party/kit-example/cmd/server/main.go
+++ examples/third-party/kit-example/cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 
 	healthService "algogrit.com/emp-server/health/service"
@@ -16,28 +15,19 @@ import (
 )
 
 func main() {
+	// Health Service on port :9000
 	r := mux.NewRouter()
-
 	hService := healthService.New()
-
 	r.HandleFunc("/healthz", hService.HealthInfo).Methods("GET")
-
-	eRepo := employeeRepository.NewInMem()
-	eService := employeeService.NewService(eRepo)
-	eEndpoints := employeeEndpoints.New(eService)
-
-	eHandler := employeeTransport.NewHTTPServer(eEndpoints)
-
-	go http.ListenAndServe(":8000", eHandler)
-
-	// r.Handle("/employees", auth.BasicAuthMiddleware(http.HandlerFunc(eService.Index))).Methods("GET")
-	// r.Handle("/employees", auth.BasicAuthMiddleware(http.HandlerFunc(eService.Create))).Methods("POST")
-	// r.Handle("/employees/{id}", auth.BasicAuthMiddleware(http.HandlerFunc(eService.Show))).Methods("GET")
-
 	n := negroni.Classic() // Includes some default middlewares
 	// n.Use(negroni.HandlerFunc(auth.BasicAuthMiddleware))
 	n.UseHandler(r)
+	go http.ListenAndServe(":9000", n)
 
-	err := http.ListenAndServe(":9000", n)
-	log.Error(err)
+	// Employee Service on port :8000
+	eRepo := employeeRepository.NewInMem()
+	eService := employeeService.NewService(eRepo)
+	eEndpoints := employeeEndpoints.New(eService)
+	eHandler := employeeTransport.NewHTTPServer(eEndpoints)
+	http.ListenAndServe(":8000", eHandler)
 }
