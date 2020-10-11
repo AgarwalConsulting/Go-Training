c407a8d27aff5d468349f94fd8c25672de55be18
Gaurav Agarwal <algogrit@gmail.com>
2020-10-11 14:09:46 +0530

    Updating examples.

--- examples/11-A-01-net/http/http-with-auth/go.mod
+++ examples/11-A-01-net/http/http-with-auth/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/golang/mock v1.4.4 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/onsi/ginkgo v1.14.0
--- examples/11-B-concurrency/channels/slow-receivers.go
+++ examples/11-B-concurrency/channels/slow-receivers.go
@@ -2,31 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func Consumer(c chan int, id int) {
-	for {
-		fmt.Println("C", id, "Processing...")
-		time.Sleep(1 * time.Second)
-		i := <-c // Receive blocks if the buffer is empty
-		fmt.Println("C", id, "Processed: ", i)
-	}
-}
-
-func main() {
-	var c chan int // nil
-	c = make(chan int, 10)
-
-	go Consumer(c, 1)
-	go Consumer(c, 2)
-
+func Producer(c chan<- int) {
 	// time.Sleep(3 * time.Second)
 	i, j := 0, 1
-	for x := 0; x < 2; x++ {
+	for {
 		i, j = j, i+j
 		nextVal := j - i
 		c <- nextVal // Sending blocks only if the buffer is full
 		fmt.Println("Sent: ", nextVal)
 	}
+}
+
+func Consumer(c <-chan int, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for x := 0; x < 2; x++ {
+		fmt.Println("C", id, "Processing...")
+		i := <-c // Receive blocks if the buffer is empty
+		// Take some time to process that value
+		time.Sleep(1 * time.Second)
+		fmt.Println("C", id, "Processed: ", i)
+	}
+}
+
+func main() {
+	var c chan int
+
+	fmt.Println(c) // <nil>
+
+	c = make(chan int)
+
+	go Producer(c)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go Consumer(c, 2, &wg)
+	go Consumer(c, 1, &wg)
+
+	wg.Wait()
 }
