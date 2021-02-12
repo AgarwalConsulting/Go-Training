3e7b7ffe21006effb6d7425b67b4711c8bf100e6
Gaurav Agarwal <algogrit@gmail.com>
2021-02-12 22:00:48 +0530

    5 Day outline!

--- create-outline-pdf.sh
+++ create-outline-pdf.sh
@@ -3,3 +3,5 @@
 npx md-to-pdf ./Agenda/draft/outline.md /tmp/outline.pdf
 
 npx md-to-pdf ./Agenda/draft/half-day.md /tmp/half-day.pdf
+
+npx md-to-pdf ./Agenda/draft/5-day-outline.md /tmp/5-day-outline.pdf
