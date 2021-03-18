565c81c4500a6bfd7c6b80c2a56e27ed5eefc7b3
Gaurav Agarwal <algogrit@gmail.com>
2021-03-18 12:26:38 +0530

    Updating copy slice example.

--- examples/04-ds/slices/copy.go
+++ examples/04-ds/slices/copy.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
+func desc(s1, s2 []int) {
+	fmt.Printf("s1: %p ; s2: %p\n", s1, s2)
+	fmt.Println("\ts1:", s1, "|\ts2:", s2)
+}
+
 func main() {
 	var s1 = []int{1, 2, 3} // Len: 3
-	// var s2 = []int{6, 7}    // Len: 2
+
 	s2 := make([]int, len(s1))
 
-	// copy(s1, s2)
-	copy(s2, s1)
+	desc(s1, s2)
 
-	fmt.Println(s1) // [1, 2, 3]
-	fmt.Println(s2) // [1, 2]
+	copy(s2, s1) // dst, src; minimum of (len(s1), len(s2)) == 0
+
+	desc(s1, s2) // s1: [1, 2, 3] ; s2: []
 }
