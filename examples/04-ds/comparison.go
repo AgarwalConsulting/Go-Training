fa2106705d98b62d993294491dc50c26fc699278
Gaurav Agarwal <algogrit@gmail.com>
2021-05-05 14:09:19 +0530

    Adding comparison example.

---
+++ examples/04-ds/comparison.go
@@ -0,0 +1,82 @@
+package main
+
+func main() {
+	// Comparing different types
+	// var f float64 = 42
+	// var i int = 42
+
+	// fmt.Println("Comparing float with int:", i == f)
+
+	// ----------
+	// Arrays
+	// ----------
+	// a := [2]int{1, 2}
+	// fmt.Printf("Address of A: %p\n", &a)
+	// b := [2]int{1, 2}
+	// fmt.Printf("Address of B: %p\n", &b)
+
+	// fmt.Println("[2]int", a == b)
+
+	// m := map[[2]int]string{}
+
+	// m[a] = "Hello"
+
+	// fmt.Println(m[b])
+
+	// ----------
+	// Slices
+	// ----------
+
+	// a := []int{1, 2}
+	// fmt.Printf("Address of A: %p\n", &a)
+	// b := []int{1, 2}
+	// fmt.Printf("Address of B: %p\n", &b)
+
+	// fmt.Println("[]int", a == b)
+
+	// m := map[[]int]string{}
+
+	// m[a] = "Hello"
+
+	// fmt.Println(m[b])
+
+	// ----------
+	// Structs
+	// ----------
+
+	// a := struct {
+	// 	x int
+	// 	y int
+	// }{10, 20}
+
+	// fmt.Printf("Type of A: %T\n", a)
+
+	// b := struct {
+	// 	x int
+	// 	y int
+	// }{10, 20}
+
+	// fmt.Printf("Type of B: %T\n", b)
+
+	// fmt.Println("structs", a == b)
+
+	// m := map[struct {
+	// 	x int
+	// 	y int
+	// }]string{}
+
+	// m[a] = "Hello"
+
+	// fmt.Println(m[b])
+
+	// ----------
+	// Maps
+	// ----------
+
+	// m1 := map[int]string{}
+	// m2 := map[int]string{}
+
+	// fmt.Println(m1 == m2)
+
+	// m := map[map[int]string]string{}
+}
