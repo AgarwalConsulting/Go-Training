c0374da3471485d01231f1dc593f6c47de5bf6d8
Gaurav Agarwal <algogrit@gmail.com>
2021-06-21 19:48:26 +0530

    Updating examples.

--- examples/03-flow-control/for.go
+++ examples/03-flow-control/for.go
@@ -6,9 +6,9 @@ import (
 
 func main() {
 	// // Normal for loop
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Println("i:", i)
-	// }
+	for i := 0; i < 10; i++ {
+		fmt.Println("i:", i)
+	}
 
 	// // for without init & post == while loop
 	// i := 0
@@ -18,14 +18,14 @@ func main() {
 	// }
 
 	// // for without any condition == forever loop
-	i := 0
-	for {
-		fmt.Println("i:", i)
-		// time.Sleep(1 * time.Second)
+	// i := 0
+	// for {
+	// 	fmt.Println("i:", i)
+	// 	// time.Sleep(1 * time.Second)
 
-		if i == 10 {
-			break
-		}
-		i++
-	}
+	// 	if i == 10 {
+	// 		break
+	// 	}
+	// 	i++
+	// }
 }
--- examples/03-flow-control/for_range.go
+++ examples/03-flow-control/for_range.go
@@ -3,25 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	str := "hello 😀"
+	// str := "hello 😀"
+	str := "नमस्ते"
 
 	// // Iterating on each byte of a string
-	// var char byte // unit8 = 8 bits
-	// for i := 0; i < len(str); i++ {
-	// 	char = str[i]
-	// 	fmt.Printf("%T %#v\n", char, string(char))
-	// }
+	var char byte // unit8 = 8 bits
+	for i := 0; i < len(str); i++ {
+		char = str[i]
+		fmt.Printf("%T %#v\n", char, string(char))
+	}
 
 	// // Iterating on each rune of a string
 	// // <for index, element := range str>
 	// var char rune // int32 = 32 bits or 4 bytes
 	// for index, char := range str {
-	// 	fmt.Printf("%d: %T %#v\n", index, char, string(char))
+	// 	fmt.Printf("%d: %T %#v %#v\n", index, char, char, string(char))
 	// }
 
-	for _, char := range str {
-		fmt.Printf("%T %#v %#v\n", char, char, string(char))
-	}
+	// for _, char := range str {
+	// 	fmt.Printf("%T %#v %#v\n", char, char, string(char))
+	// }
 }
 
 // Range works on:
