4dba0193e59afff92409153fcfe545d051e0d333
Gaurav Agarwal <algogrit@gmail.com>
2021-07-08 09:27:57 +0530

    Adding facade example.

--- patterns/design/2-structural/05-facade/facade.go
+++ patterns/design/2-structural/05-facade/facade.go
@@ -1,1 +1,24 @@
-// Facade: Working with BankAccounts, Wallets, SecurityCode, Ledger, Notification types using a Facade Eg: https://refactoring.guru/design-patterns/facade/go/example#example-0
+// Code take from: https://refactoring.guru/design-patterns/facade/go/example#example-0
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
