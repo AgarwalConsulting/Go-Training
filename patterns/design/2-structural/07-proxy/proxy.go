1c122058f018d9ba4bba8d7ea4b00dff6b4aa258
Gaurav Agarwal <algogrit@gmail.com>
2021-07-08 09:47:09 +0530

    Adding prompt for proxy.

---
+++ patterns/design/2-structural/07-proxy/proxy.go
@@ -0,0 +1,2 @@
+// Protection Proxy: Car with Drive method; Driver with Age; CarProxy with car and driver with Drive method as well.
+// Virtual Proxy: Image interface with Draw, Bitmap with Draw method & NewBitmap, DrawImage(image Image); Introduce LazyBitmap with Draw method & NewLazyBitmap
