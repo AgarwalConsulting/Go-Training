20b7b00b2da4bd424c72600a58b790c109db3ba9
Gaurav Agarwal <algogrit@gmail.com>
2021-09-07 10:26:16 +0530

    Using contains instead.

--- patterns/concurrency/examples/word_count.go
+++ patterns/concurrency/examples/word_count.go
@@ -61,7 +61,7 @@ func splitIntoSentences(text string) <-chan string {
 		var sentence = []rune{}
 
 		for _, el := range text {
-			if el == '.' || el == '?' || el == '!' || el == ';' {
+			if strings.Contains(`.,?!;()"`, string(el)) {
 				if len(sentence) > 0 {
 					result <- string(sentence)
 				}
@@ -83,9 +83,9 @@ func splitIntoSentences(text string) <-chan string {
 func main() {
 	text := `Parallelism is about doing things in parallel that can also be done sequentially. A common example is counting the frequency of letters. Create a function that returns the total frequency of each letter in a list of texts and that employs parallelism.
 
-	Go supports concurrency via goroutines which are started with the go keyword. It is a simple, lightweight and elegant way to provide concurrency support and is one of the greatest strengths of the language.
+	Go supports concurrency via "goroutines" which are started with the go keyword. It is a simple, lightweight and elegant way to provide concurrency support and is one of the greatest strengths of the language.
 
-	You may notice that while this exercise is called Parallel letter frequency you don't see the term Parallel used very often in Go. Gophers prefer to use the term Concurrent to describe the management of multiple independent goroutines. Although these terms are often used interchangeably Gophers like to be technically correct and use concurrent when discussing the seemingly simultaneous executions of goroutines.
+	You may notice that while this exercise is called Parallel letter frequency you don't see the term "Parallel" used very often in Go. Gophers prefer to use the term Concurrent to describe the management of multiple independent goroutines ("processes" or "threads" in other language contexts). Although these terms are often used interchangeably Gophers like to be technically correct and use "concurrent" when discussing the seemingly simultaneous executions of goroutines.
 
 	While we can plan for our programs to run in parallel, and at times they may appear to run in parallel, without strict knowledge of the execution context of our code all we can guarantee is that processes will run concurrently. In other words they may be executing sequentially faster than we can distinguish but not strictly simultaneously.`
 
