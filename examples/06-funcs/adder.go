3d450de21860fd289508b42e9e5aa78b52981801
Gaurav Agarwal <algogrit@gmail.com>
2021-09-16 12:49:35 +0530

    Updating adder starter.

--- examples/06-funcs/adder.go
+++ examples/06-funcs/adder.go
@@ -3,19 +3,13 @@ package main
 import "fmt"
 
 func Adder(n int) func(int) int {
-	return func(x int) int { // x = 10; n = 2
-		return x + n // Property of closures: Capture outside scope
+	return func(x int) int {
+		return x + n
 	}
 }
 
 func main() {
 	var addBy2 func(int) int
-
-	// f := Adder
-
-	// fmt.Printf("%T\n", f) // func(int) func(int) int
-
-	// addBy2 = Adder // ?
 
 	fmt.Println(addBy2)
 
@@ -23,5 +17,5 @@ func main() {
 
 	fmt.Printf("%T\n", addBy2)
 
-	fmt.Println(addBy2(10)) //
+	fmt.Println(addBy2(10))
 }
