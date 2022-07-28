3e9b7bbc11b862e36ea6320a588a22521d880e0a
Gaurav Agarwal <algogrit@gmail.com>
2022-07-28 23:58:27 +0530

    Removing duplicate scripts.

--- examples/20-operators/operator-sdk/scripts/create-api.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd myoperator
-operator-sdk create api --group=myplatform --version=v1alpha1 --kind=InhouseApp
--- examples/20-operators/operator-sdk/scripts/scaffold.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# https://sdk.operatorframework.io/docs/installation/
-
-rm -rf myoperator
-mkdir myoperator
-cd myoperator
-operator-sdk init --domain=algogrit.com --repo=github.com/AgarwalConsulting/myplatform operator --skip-go-version-check
