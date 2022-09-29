d705714dc8fe26418f255dbb8cca6133d871515e
Gaurav Agarwal <algogrit@gmail.com>
2022-09-29 18:22:30 +0530

    Updating strings example.

--- examples/04-ds/strings.go
+++ examples/04-ds/strings.go
@@ -3,22 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	str := "hello" // Type: string
-
+	str := "नमस्ते" // Na-Ma-S-Te (Grapheme Clustering)
 	// str := "hello 😀"
-	// str := "नमस्ते"
+	// str := "hello" // Type: string
 
-	fmt.Println("Length:", len(str)) // 5 bytes
+	fmt.Println("Length:", len(str)) // 18 bytes
 
-	var b []byte = []byte(str)
+	// fmt.Println(str[0:7]) // "hello 😀" or "hello "
+
+	// fmt.Printf("%d\n", '😀')
+
+	var b []byte = []byte(str) // byte -> uint8 (1 byte -> 8 bits)
+
+	// How much memory would b take: 10 bytes
 
 	fmt.Println(b)
 
-	fmt.Println(str[0:5])
-	fmt.Println(b[0:5])
-	fmt.Println(string(b[0:5]))
+	fmt.Println(len(b)) // Length of b: 10
 
-	for idx, char := range str {
-		fmt.Printf("\tChar at %d: %s (%d)", idx, string(char), char)
+	// fmt.Println(b[0:7])
+	// fmt.Println(string(b[0:7]))
+
+	var r []rune = []rune(str) // rune -> int32 (4 bytes) // Unicode-code point
+
+	fmt.Println("No. of characters:", len(r)) // 6
+
+	fmt.Println(r)
+
+	// fmt.Println(r[0:7])
+	// fmt.Println(string(r[0:7]))
+
+	for idx, el := range r {
+		fmt.Printf("\t%d: %d - %s\n", idx, el, string(el))
 	}
 }
