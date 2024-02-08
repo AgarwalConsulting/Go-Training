df90c9d0e7711ca874863e979104ffa91ceecb5b
Gaurav Agarwal <algogrit@gmail.com>
2024-02-08 14:10:53 +0530

    Stack code: debugging required.

---
+++ examples/04-ds/stack.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack[T any] []T
+
+func (s *Stack[T]) Push(el ...T) {
+	*s = append(*s, el...)
+}
+
+func (s *Stack[T]) Pop() (T, error) {
+	if len(*s) == 0 {
+		return *new(T), errors.New("last element")
+	}
+	lastEl := (*s)[len(*s)-1]
+
+	*s = (*s)[0 : len(*s)-1]
+
+	return lastEl, nil
+}
+
+func NewStack[T any]() Stack[T] {
+	return make(Stack[T], 0, 10)
+}
+
+func main() {
+	s := NewStack[int]()
+
+	s.Push(1, 2, 3, 5, 7)
+
+	for el, err := s.Pop(); err == nil; {
+		fmt.Println("Popped:", el)
+	}
+}
