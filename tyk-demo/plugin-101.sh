102fa623f10d634bf5fc519b3864afb2984683c9
Gaurav Agarwal <algogrit@gmail.com>
2024-02-09 18:23:07 +0530

    Running from the dir.

--- tyk-demo/plugin-101.sh
+++ tyk-demo/plugin-101.sh
@@ -9,7 +9,7 @@ go get github.com/TykTechnologies/tyk@ffa83a27d3bf793aa27e5f6e4c7106106286699d
 go mod tidy
 
 
-# Building the plugin
+# Building the plugin: From `cd tyk-plugin`
 # docker pull tykio/tyk-plugin-compiler:v5.2.1
 # docker run --rm -v `pwd`:/plugin-source \
 #            --platform=linux/amd64 \
