2f2a3e0b6d48c5f65b00d7a05ab7670a75f9be3b
Gaurav Agarwal <algogrit@gmail.com>
2024-04-15 10:58:22 +0530

    Updating the context example.

--- examples/11-B-concurrency/context/context.go
+++ examples/11-B-concurrency/context/context.go
@@ -3,41 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func fibonacci(fibCtx context.Context, c chan int) {
+// func fibonacci(id int, c chan<- int, quit <-chan int, wg *sync.WaitGroup) {
+func fibonacci(ctx context.Context, id int, c chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	if id < 10 {
+		childCtx, _ := context.WithCancel(ctx)
+		wg.Add(1)
+		go fibonacci(childCtx, id+10, c, wg)
+	}
 	x, y := 0, 1
-
-	// helloCtx, cancelFn := context.WithCancel(fibCtx)
-	// defer cancelFn()
-
-	for {
+	for { // Forever!
 		select {
-		case c <- x: // send
+		case c <- x: // Sending to the data channel
+			fmt.Println("Sent next value!", id)
 			x, y = y, x+y
-		case <-fibCtx.Done(): // receive
-			fmt.Println("Returning from fibonacci...")
+		case <-ctx.Done(): // Receiving from a closed channel
+			fmt.Println("!!!quiting...!!! ->", id)
 			return
+		default:
+			fmt.Println("---Waiting for receiver...", id)
+			time.Sleep(time.Millisecond * 900)
 		}
 	}
 }
 
 func main() {
-	c := make(chan int)
+	c := make(chan int) // Data channel // Unbuffered
 
-	ctx := context.Background()
+	ctx, cancelFn := context.WithCancel(context.Background())
 
-	fibCtx, _ := context.WithTimeout(ctx, 100*time.Microsecond)
-
-	// Consumer
-	go func() {
-		for {
-			fmt.Println(<-c)
+	// quit := make(chan int) // Signal channel
+	go func() { // Consumer
+		defer fmt.Println("Terminating consumer...")
+		for i := 0; i < 10; i++ {
+			fmt.Println("Received", i, "th value: ", <-c)
+			time.Sleep(1 * time.Second)
 		}
+		// close(quit)
+		cancelFn()
 	}()
 
-	fibonacci(fibCtx, c)
+	var wg sync.WaitGroup
+	wg.Add(3)
 
-	time.Sleep(2 * time.Second)
+	go fibonacci(ctx, 1, c, &wg) // Producer
+	go fibonacci(ctx, 2, c, &wg) // Producer
+	go fibonacci(ctx, 3, c, &wg) // Producer
+
+	wg.Wait()
 }
