fccc172420ebec4062a6e71bdb6a512988e8412f
Gaurav Agarwal <algogrit@gmail.com>
2024-05-13 12:13:33 +0530

    Updating the iterator race code.

--- examples/11-B-concurrency/iterator_race/main.go
+++ examples/11-B-concurrency/iterator_race/main.go
@@ -9,9 +9,10 @@ func main() {
 	for i := 0; i < 3; i++ {
 		// go fmt.Println(i)
 		// k := i
-		go func(k int) {
-			fmt.Println(k)
-		}(i)
+		go func() {
+			fmt.Println(i) // Closure Property
+		}()
+		// time.Sleep(time.Microsecond)
 	}
 	time.Sleep(1 * time.Second)
 }
